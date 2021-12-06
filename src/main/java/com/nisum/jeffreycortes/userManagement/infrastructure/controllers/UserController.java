13d230aa5d6bf5873f2bf533fbf269dcb4e5ac40
Jeffrey Cortes <cort801416@siigo.com>
2021-12-05 20:05:04 -0500

    Se configura validación para createUser de UserController

--- src/main/java/com/nisum/jeffreycortes/userManagement/infrastructure/controllers/UserController.java
+++ src/main/java/com/nisum/jeffreycortes/userManagement/infrastructure/controllers/UserController.java
@@ -3,8 +3,12 @@ package com.nisum.jeffreycortes.userManagement.infrastructure.controllers;
 import com.nisum.jeffreycortes.userManagement.aplication.dtos.UserCreatedResponse;
 import com.nisum.jeffreycortes.userManagement.aplication.dtos.UserRequestCreateDto;
 import com.nisum.jeffreycortes.userManagement.aplication.services.UserService;
+import com.sun.media.sound.InvalidDataException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -19,7 +23,7 @@ public class UserController {
     }
 
     @PostMapping
-    public UserCreatedResponse createUser(@RequestBody UserRequestCreateDto userDto) throws Exception {
+    public UserCreatedResponse createUser(@Valid @RequestBody UserRequestCreateDto userDto, BindingResult result) {
         return userService.Create(userDto);
     }
 }
