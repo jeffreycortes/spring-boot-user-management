ad78dfe42933ce9aeb29d1c51daca31ea731ed98
Jeffrey Cortes <cort801416@siigo.com>
2021-12-05 21:37:05 -0500

    Se quita import de List innecesario de User

--- src/main/java/com/nisum/jeffreycortes/userManagement/domain/User.java
+++ src/main/java/com/nisum/jeffreycortes/userManagement/domain/User.java
@@ -11,7 +11,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.Instant;
-import java.util.List;
 import java.util.Set;
 
 @Entity
